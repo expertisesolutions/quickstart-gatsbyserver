8eccc9623290a474c8fdbd0febf4e784d4bc3b80
ricardocvel <ricardocampos_pr@live.com>
2021-03-19 12:58:40 -0300

    adding components to the login structure

---
+++ src/components/RegisterForm/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function RegisterForm() {
+    return(
+        <Container>
+            <h1>RegisterForm</h1>
+        </Container>
+    );
+}
