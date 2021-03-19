db0d05dee15f9b3d78fd19cc0afa5b07166f95f6
ricardocvel <ricardocampos_pr@live.com>
2021-03-19 17:43:25 -0300

    adding global styling settings

--- src/styles/themes/light.js
+++ src/styles/themes/light.js
@@ -1,4 +1,26 @@
 export default {
-  background: '#fff',
-  primary: '#323233'
-};
+    //colors
+    background: '#fff',
+    primary: '##ebebeb',
+    secondary: '#fff',
+    tertiary: '#fff',
+    contrast: '#fff',
+
+    fontEmphasis: '#555555',
+    fontPrimary: '#000',
+    fontSecondary: '#000',
+
+    borderPrimay: '#555555',
+    borderSecondary: '#000',
+
+
+    //Fonte
+    fontSmall: '10px',
+    fontMedium: '16px',
+    fontBig: '20px',
+    fontTitle: '30px',
+    fontSubtitle: '20px',
+    fontFeatured: 'bold',
+
+
+}
