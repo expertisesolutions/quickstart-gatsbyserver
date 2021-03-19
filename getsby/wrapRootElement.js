ea6fbb4203982db367727e138c838f993b72620a
ricardocvel <ricardocampos_pr@live.com>
2021-03-19 12:55:28 -0300

    setting bassecss in wrapRootElement

--- getsby/wrapRootElement.js
+++ getsby/wrapRootElement.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import light from '../src/styles/themes/light';
-
-//https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
+import { BaseCSS } from '../src/styles/BaseCSS';
 
 export function wrapRootElement ({ element }) {
-  return <ThemeProvider theme={light}>{element}</ThemeProvider>;
+  return (
+  <ThemeProvider theme={light}>
+    <BaseCSS/>
+    {element}
+  </ThemeProvider>
+  );
 }
