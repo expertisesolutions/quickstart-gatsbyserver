2d0917fee98212572c425d5482c8f29990ef74ef
ricardocvel <ricardocampos_pr@live.com>
2021-03-24 17:09:25 -0300

    pattern and indent corrections

--- src/auth/privateComponent/index.js
+++ src/auth/privateComponent/index.js
@@ -1,15 +1,14 @@
-import React from "react"
-import { navigate } from "gatsby"
-//import { isAuthenticated } from "../../auth/auth"
-const isAuthenticated = true //temporario
-
+import React from 'react';
+import { navigate } from 'gatsby';
+// import { isAuthenticated } from "../../auth/auth"
+const isAuthenticated = true; // temporario
 
 //
 const PrivateRoute = ({ component: Component, ...rest }) => {
   if (!isAuthenticated) {
-    navigate("/login/")
-    return null
+    navigate('/login/');
+    return null;
   }
-  return <Component {...rest} />
-}
-export default PrivateRoute
+  return <Component {...rest} />;
+};
+export default PrivateRoute;
