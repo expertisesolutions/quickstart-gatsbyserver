209e6c1265c11d71433d33abe56a311714557bff
ricardocvel <ricardocampos_pr@live.com>
2021-03-31 16:29:11 -0300

    added dockerfile prod

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import { Container } from './styles';
-import Private from '../../auth/privateComponent'
-import Teste from '../../components/teste'
 
 export default function Page01() {
   return (
         <Container>
-            <Private component={Teste}/>
+          <h1>dd</h1>
         </Container>
   );
 }
