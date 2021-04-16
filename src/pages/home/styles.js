31869c65326a470582976694079bdbb7d5afb441
ricardocvel <ricardocampos_pr@live.com>
2021-04-16 15:00:52 -0300

    building home page

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,5 +1,24 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    color: ${props => props.theme.primary}
+export const Limiter = styled.div`
+        width: 100%;
+        margin: 0 auto;
+        background-color: ${props => props.theme.primary};
     `;
+
+export const Container = styled.div`
+    width: 100%;  
+    min-height: 100vh;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: flex;
+    flex-direction: column;
+    //flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 15px;
+
+`;
